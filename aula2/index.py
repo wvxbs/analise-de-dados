7baee50db0b9d83c50d32e0b348b3fe6c42d625b
wvxbs <gabriel.ferreira7854@gmail.com>
2024-09-14 17:06:10 -0300

    .

---
+++ aula2/index.py
@@ -0,0 +1,12 @@
+dicionario_01 = {"Indice" : "valor", "bolsonaro": "guerreiro"}
+dicionario_01
+
+Nome = "Gabriel"
+Sobrenome = "Ferreira"
+
+nomeCompleto = Nome + Sobrenome
+
+while true:
+    nome = "Gabriel"
+    idade = 23
+
